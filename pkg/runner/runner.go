771d32cee42aa465a35efff595b1afb78d80acb0
Jacek Wysocki <jacek.wysocki@gmail.com>
2022-02-23 12:54:53 +0100

    fix: added error handling

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -31,8 +31,8 @@ func (r *ExampleRunner) Run(execution testkube.Execution) (result testkube.Execu
 	path := os.Getenv("RUNNER_DATADIR")
 
 	// let's print content of passed volume
-	output.PrintEvent("path:", path)
-	filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+	output.PrintEvent("found files in:", path)
+	err = filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -40,23 +40,31 @@ func (r *ExampleRunner) Run(execution testkube.Execution) (result testkube.Execu
 		if err != nil {
 			return err
 		}
-		output.PrintEvent("- file: ", path, string(d))
+		output.PrintEvent("- ", path, string(d))
 		return nil
 
 	})
+
+	if err != nil {
+		return result, err
+	}
 
 	output.PrintLog("some log message from executor")
 
 	// e.g. Cypress test is stored in Git repo so Testkube will checkout it automatically
 	// and allow you to use it easily
 	// we can create test like below:
-	// $ echo "https://httpstat.us/200" | kubectl testkube tests create --name example-google-test --type example/test
+	// $ echo "https://httpstat.us/200" | kubectl testkube tests create --name example-test --type example/test
 	uri := strings.TrimSuffix(execution.Content.Data, "\n") // newline on the end is not needed :)
 
 	// other way to get data could be load it from Git e.g. file in git repo
 	// if test will be created using git file content (like this one example with uri stored in file https://github.com/kubeshop/testkube-executor-example/blob/88f6ad4b4317a5a17bf6cd70bb41c4622d3ccfef/examples/test-definition-file)
+	// e.g.
+	// $ kubectl testkube tests create --name example-test-git --type example/test --git-uri https://github.com/kubeshop/testkube-executor-example.git --git-path examples/test-definition-file --git-branch jacek/fix/custom-executor-example-update
 	// Testkube will automatically map this dir/file to directory defined in the RUNNER_DATADIR env variable
+	// so you'll need to handle it here (if it's not handled by your test framework already e.g. Cypress runs in root level of test project)
 
+	// below is test our test against passed URI status code
 	resp, err := http.Get(uri)
 	if err != nil {
 		return result, err
